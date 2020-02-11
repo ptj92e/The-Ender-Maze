c70fc89a6b9d009699477cd0966408ce6d029d3d
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-02-10 23:16:11 -0600

    more class updates, paladin/wizard classes finished with presets
    Have the paladin and wizard classes finished with presets, still need to add other cases to castSpell and meleeAttack so that when the user levels, the case is already there for the new spells and attacks that are aquired on level up

--- public/assets/js/createCharacter.js
+++ public/assets/js/createCharacter.js
@@ -5,17 +5,34 @@ let wizard1 = new Wizard("Minthel");
 let paladin1 = new Paladin("Lorash");
 
 console.log(wizard1);
-console.log(paladin1);
+
 
 //just testing the castSpell function on something to see if the damage calculates correctly and updates the targets health correctly. Will actually have Enemy class thats created from a list of enemies, with stats being randomly generated based on user character level
 let enemy = {
     name: "Goblin",
     health: 100,
-    experience_value: 100
+    experience_value: 100,
+    speed: 10
 }
-console.log(`${enemy.name} Health: ${enemy.health}`);
+
+/**
+ * Going to keep below statements in for combat simulation, need to figure out how to actually handle this on the front end, when attacks and such are selected from the menu
+ */
+
+//when handling combat, this is how we should do both health pools, so that we don't get repeated decimals (possible to also just round all the damage down and just make everything whole numbers, will get into that when needed, for now will just think of doing it this way)
+let enemy_health = enemy.health.toFixed(2);
+
+console.log(`${enemy.name} Health: ${enemy_health}`);
 wizard1.castSpell("Fireball", enemy);
-console.log(`${enemy.name} Health: ${enemy.health}`);
+console.log(`${enemy.name} Health: ${enemy_health}`);
+
+paladin1.castSpell("Judgement", enemy);
+console.log(`${enemy.name} Health: ${enemy_health}`);
 
 
+wizard1.castSpell("Frostbolt", enemy);
+console.log(`${enemy.name} Health: ${enemy_health}`);
+console.log(`${enemy.name}'s speed has been reduced to ${enemy.speed}`);
 
+wizard1.basicAttack(wizard1.weapon, enemy);
+console.log(`${enemy.name} Health: ${enemy_health}`);
