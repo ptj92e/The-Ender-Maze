7158f76b6c9b33c9083715a0344f5d26537e102d
Phillip Jones <ptj92e@gmail.com>
2020-02-12 12:23:02 -0600

    Updated handlebars and added click listeners to create buttons.

---
+++ public/assets/js/maze.js
@@ -0,0 +1,20 @@
+//Adding click listeners to create buttons
+$("#paladin").on("click", function () {
+    let paladinName = $("#paladinName").val().trim();
+    
+});
+
+$("#cleric").on("click", function () {
+    let clericName = $("#clericName").val().trim();
+   
+});
+
+$("#rogue").on("click", function () {
+    let rogueName = $("#rogueName").val().trim();
+    
+});
+
+$("#wizard").on("click", function () {
+    let wizardName = $("#wizardName").val().trim();
+    
+});
