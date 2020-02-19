ff138e73d0b5dd4053cbdd98c024fd920fd85f14
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-02-18 20:33:56 -0600

    staging to approve pr

---
+++ domain/classes/enemyAPICall.js
@@ -0,0 +1,6 @@
+const axios = require("axios");
+
+//set global empty array, so I can perform axios call, retrieve all the names from the list, and pipe them into this array for export
+const enemy_names = [];
+
+
